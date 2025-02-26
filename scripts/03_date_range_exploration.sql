b5c369d3b807e1b269df0599d998c80cf0d3bdb3
Gisela Campagno <giselacampagno@gmail.com>
2025-02-26 11:25:53 -0300

    Create 03_date_range_exploration.sql

---
+++ scripts/03_date_range_exploration.sql
@@ -0,0 +1,15 @@
+-- Finding the date of the fist and last order
+-- How many years of sales are available
+SELECT 
+	MIN(order_date) AS first_order_date,
+    MAX(order_date) AS last_order_date,
+    TIMESTAMPDIFF(YEAR, MIN(order_date), MAX(order_date)) AS years_of_sales_available
+FROM gold2.fact_sales; 
+
+-- Finding the youngest and oldest customer
+SELECT 
+	MIN(birthdate) AS 'Youngest birthdate',
+	TIMESTAMPDIFF(YEAR, MIN(birthdate), NOW()) AS 'Oldest customer age',
+    MAX(birthdate) AS 'Oldest birthdate',
+	TIMESTAMPDIFF(YEAR, MAX(birthdate), NOW()) AS 'Youngest customer age'
+FROM gold2.dim_customers;
