8022d9acef895ad3bd87dfdf51edc6dadad9a59b
Gisela Campagno <giselacampagno@gmail.com>
2025-02-27 09:56:11 -0300

    Update 07_change_over_time.sql

--- scripts/07_change_over_time.sql
+++ scripts/07_change_over_time.sql
@@ -1,3 +1,18 @@
+/*
+===============================================================================
+Change Over Time Analysis
+===============================================================================
+Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+
+SQL Functions Used:
+    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+===============================================================================
+*/
+
 -- Analize sales performance over time
 -- Over the years
 SELECT 
