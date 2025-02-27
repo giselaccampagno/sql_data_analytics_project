4e2d2ee99c2fd6ae9943de88bd122ee5da9e32e4
Gisela Campagno <giselacampagno@gmail.com>
2025-02-27 09:57:31 -0300

    Update 04_messures_exploration.sql

--- scripts/04_messures_exploration.sql
+++ scripts/04_messures_exploration.sql
@@ -1,3 +1,16 @@
+/*
+===============================================================================
+Measures Exploration (Key Metrics)
+===============================================================================
+Purpose:
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
+
+SQL Functions Used:
+    - COUNT(), SUM(), AVG()
+===============================================================================
+*/
+
 -- Find the Total Sales
 SELECT SUM(sales) AS total_sales FROM gold2.fact_sales;
 
