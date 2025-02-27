8c5092ab008bba03e9e2f348e682d27860f5212d
Gisela Campagno <giselacampagno@gmail.com>
2025-02-27 09:57:04 -0300

    Update 05_magnitude_analysis.sql

--- scripts/05_magnitude_analysis.sql
+++ scripts/05_magnitude_analysis.sql
@@ -1,3 +1,17 @@
+/*
+===============================================================================
+Magnitude Analysis
+===============================================================================
+Purpose:
+    - To quantify data and group results by specific dimensions.
+    - For understanding data distribution across categories.
+
+SQL Functions Used:
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+    - GROUP BY, ORDER BY
+===============================================================================
+*/
+
 -- Finding total customers by countries
 SELECT
     country,
