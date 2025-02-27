0cc902f08cb3b3f8acf19cd162553c108f7b203e
Gisela Campagno <giselacampagno@gmail.com>
2025-02-27 08:36:59 -0300

    Create 07_change_over_time.sql

---
+++ scripts/07_change_over_time.sql
@@ -0,0 +1,33 @@
+-- Analize sales performance over time
+-- Over the years
+SELECT 
+	YEAR(order_date) AS year,
+    SUM(sales) AS total_revenue,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold2.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY year
+ORDER BY year;
+
+-- Over the months
+SELECT 
+	MONTH(order_date) AS month,
+    SUM(sales) AS total_revenue,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold2.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY month
+ORDER BY month;
+
+-- Over years and months
+SELECT 
+	DATE_FORMAT(order_date, '%Y-%m-01') AS order_date,
+    SUM(sales) AS total_revenue,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold2.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY order_date
+ORDER BY order_date;
